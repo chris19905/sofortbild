46ec4db3383d1c66e52d30bb8feb2f495f395c01
chris19905 <57181378+chris19905@users.noreply.github.com>
2026-09-21 19:35:05 +0200

    Add files via upload
    1.1.3 Bug fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sofortbild-v4';
+var CACHE = 'sofortbild-v5';
 var CORE = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon.svg'];
 
 self.addEventListener('install', function(e){
