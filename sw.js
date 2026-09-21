7ab3b0cf0ebd22be99295641013cd7a988533e85
chris19905 <57181378+chris19905@users.noreply.github.com>
2026-09-21 19:17:07 +0200

    Add files via upload
    1.1.2 Bug fixing and 3D gloss

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sofortbild-v3';
+var CACHE = 'sofortbild-v4';
 var CORE = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon.svg'];
 
 self.addEventListener('install', function(e){
