1542c05892da0e1659c1e325cab0683dd7f18133
chris19905 <57181378+chris19905@users.noreply.github.com>
2026-09-21 19:50:08 +0200

    Add files via upload
    1.1.4 English added

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sofortbild-v5';
+var CACHE = 'sofortbild-v6';
 var CORE = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon.svg'];
 
 self.addEventListener('install', function(e){
