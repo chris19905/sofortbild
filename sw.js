4defdf1fb8f9936630e90f90f52e158a405ce745
chris19905 <57181378+chris19905@users.noreply.github.com>
2026-09-21 18:45:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sofortbild-v1';
+var CACHE = 'sofortbild-v2';
 var CORE = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon.svg'];
 
 self.addEventListener('install', function(e){
@@ -13,19 +13,34 @@ self.addEventListener('activate', function(e){
 
 self.addEventListener('fetch', function(e){
   if(e.request.method !== 'GET') return;
+
+  var isPage = e.request.mode === 'navigate' || /\.html($|\?)/.test(e.request.url);
+
+  if(isPage){
+    // Erst das Netz fragen, damit eine neu hochgeladene Fassung sofort ankommt
+    e.respondWith(
+      fetch(e.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copy); }).catch(function(){});
+        return res;
+      }).catch(function(){
+        return caches.match(e.request).then(function(hit){ return hit || caches.match('./index.html'); });
+      })
+    );
+    return;
+  }
+
+  // Alles andere (Icons, Schriften) darf aus dem Cache kommen
   e.respondWith(
     caches.match(e.request).then(function(hit){
       if(hit) return hit;
       return fetch(e.request).then(function(res){
-        // Schriften und eigene Dateien nebenbei mitnehmen, damit die App offline laeuft
         if(res && (res.ok || res.type === 'opaque')){
           var copy = res.clone();
           caches.open(CACHE).then(function(c){ c.put(e.request, copy); }).catch(function(){});
         }
         return res;
-      }).catch(function(){
-        return e.request.mode === 'navigate' ? caches.match('./index.html') : Response.error();
-      });
+      }).catch(function(){ return Response.error(); });
     })
   );
 });
